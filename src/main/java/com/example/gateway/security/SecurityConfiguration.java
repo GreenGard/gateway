cd05a80d1036e49cc2bc4d565ff325dd7aeee194
Madelene <you@example.com>
2022-05-22 14:48:36 +0200

    Initital commit

---
+++ src/main/java/com/example/gateway/security/SecurityConfiguration.java
@@ -0,0 +1,55 @@
+package com.example.gateway.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+import reactor.core.publisher.Mono;
+import com.example.gateway.auth.AuthenticationManager;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Value("${path.matchers.authenticate:/**}")
+    String[] authenticationMatchers;
+
+    @Value("${path.matchers.permit:/auth/**}")
+    String[] permittedMatchers;
+
+    private final AuthenticationManager authenticationManager;
+    private final SecurityContextRepository securityContextRepository;
+
+    public SecurityConfiguration(AuthenticationManager authenticationManager, SecurityContextRepository securityContextRepository) {
+        this.authenticationManager = authenticationManager;
+        this.securityContextRepository = securityContextRepository;
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+        return http
+                .httpBasic().disable()
+                .csrf().disable()
+                .logout().disable()
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+                .authenticationManager(authenticationManager)
+                .securityContextRepository(securityContextRepository)
+                .exceptionHandling()
+                .authenticationEntryPoint((swe, ex) -> Mono.fromRunnable(
+                        () -> swe.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED)))
+                .accessDeniedHandler((swe, ex) -> Mono.fromRunnable(
+                        () -> swe.getResponse().setStatusCode(HttpStatus.FORBIDDEN)))
+                .and()
+                .authorizeExchange()
+                .pathMatchers("/auth/**").permitAll()
+                .pathMatchers(authenticationMatchers).authenticated()
+                .pathMatchers(permittedMatchers).permitAll()
+                .anyExchange().hasRole("ADMIN")
+                .and().build();
+    }
+}
